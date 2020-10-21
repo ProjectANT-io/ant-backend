0a804e767d1cd457ad232b662087bc7a34a0e798
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-10-21 10:28:11 -1000

    Updated Dockerfile, Build Steps

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { UserController } from "./controller/UserController";
 
-export const Routes = [
+const Routes = [
   // {
   //   method: "get",
   //   route: "/users",
@@ -26,3 +26,5 @@ export const Routes = [
     action: "remove",
   },
 ];
+
+export default Routes;

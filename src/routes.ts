02758f92f514813915594b2396d87a983a821757
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-10-29 16:34:07 -1000

    Added POST User Create

--- src/routes.ts
+++ src/routes.ts
@@ -1,29 +1,11 @@
 import UserController from "./controller/UserController";
 
 const Routes = [
-  // {
-  //   method: "get",
-  //   route: "/users",
-  //   controller: UserController,
-  //   action: "getAllUsers",
-  // },
-  {
-    method: "get",
-    route: "/users/:user_id",
-    controller: UserController,
-    action: "getOneUser",
-  },
   {
     method: "post",
     route: "/users",
     controller: UserController,
-    action: "save",
-  },
-  {
-    method: "delete",
-    route: "/users/:id",
-    controller: UserController,
-    action: "remove",
+    action: "newUser",
   },
 ];
 

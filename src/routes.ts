4fc0abedda2483d62470c408303293af06902d9f
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-10-31 11:13:33 -1000

    Added All Route Definitions

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,63 @@
 import UserController from "./controller/UserController";
+import ProjectController from "./controller/ProjectController";
 
 const Routes = [
+  // User Routes
   {
     method: "post",
     route: "/users",
     controller: UserController,
     action: "newUser",
+  },
+  {
+    method: "get",
+    route: "/users/:user_id",
+    controller: UserController,
+    action: "getUser",
+  },
+  {
+    method: "post",
+    route: "/users/:user_id",
+    controller: UserController,
+    action: "updateUser",
+  },
+  {
+    method: "delete",
+    route: "/users/:user_id",
+    controller: UserController,
+    action: "deleteUser",
+  },
+
+  // Project Routes
+  {
+    method: "post",
+    route: "/projects/accept/:project_id",
+    controller: ProjectController,
+    action: "acceptUserForProject",
+  },
+  {
+    method: "post",
+    route: "/projects",
+    controller: ProjectController,
+    action: "createProject",
+  },
+  {
+    method: "get",
+    route: "/projects/:project_id",
+    controller: ProjectController,
+    action: "getProject",
+  },
+  {
+    method: "post",
+    route: "/projects/:project_id",
+    controller: ProjectController,
+    action: "updateProject",
+  },
+  {
+    method: "delete",
+    route: "/projects/:project_id",
+    controller: ProjectController,
+    action: "deleteProject",
   },
 ];
 

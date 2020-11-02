1492c7ceaeca04673dc46422ab8001e2c1d92381
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-11-01 22:17:34 -1000

    Added Interfaces + Schemas for Company + Project

---
+++ src/entity/ICompany.ts
@@ -0,0 +1,11 @@
+/* eslint-disable camelcase */
+export default interface ICompany {
+  id: number;
+  name: string;
+  logo: string;
+  headline: string;
+  description: string;
+  location: string;
+  industry: string;
+  company_culture: string;
+}

dbe4a799922339bd55e809187c0461a49e5c8117
Patrick Louis <patricklouisw@gmail.com>
2020-11-01 19:37:07 -0500

    Fix Connection, Update newUser and getUser

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,9 +1,9 @@
-import { getRepository } from "typeorm";
+import { getConnection, getRepository } from "typeorm";
 import { NextFunction, Request, Response } from "express";
 import User from "../entity/User";
 
 class UserController {
-  private userRepository = getRepository(User);
+  private userRepository = getConnection().getRepository(User);
 
   async authCheck(request: Request, response: Response) {
     return true; // TODO
@@ -13,6 +13,7 @@ class UserController {
     return true; // TODO
   }
 
+  // === START OF REFERENCE ===
   // async all(request: Request, response: Response, next: NextFunction) {
   //   return this.userRepository.find();
   // }
@@ -29,23 +30,62 @@ class UserController {
   //   let userToRemove = await this.userRepository.findOne(request.params.id);
   //   // await this.userRepository.remove(userToRemove);
   // }
+  // === END OF REFERENCE ===
 
-  async newUser(req: Request, res: Response) {
+  // === GET: get user by id ===
+  async getUser(req: Request, res: Response) {
+    console.log("Check userRepository: " + this.userRepository);
+
+    console.log("authCheck");
     if (!(await this.authCheck(req, res))) {
       res.status(401);
       return "Unauthorized";
     }
+    console.log("permissionsCheck");
+    if (!this.permissionsCheck(req, res)) {
+      res.status(403);
+      return "Wrong permissions";
+    }
+    
+    let foundUser;
+
+    try{
+      foundUser = this.userRepository.findOne({ id: req.params.user_id });
+    } catch (e){
+      onError(e);
+      // return e.message;
+    }
+    
+    return foundUser;
+  }
+
+  // === POST: New User to DB ===
+  async newUser(req: Request, res: Response) {
+
+    // console.log("Check userRepository: " + this.userRepository);
+
+    // console.log("authCheck");
+    if (!(await this.authCheck(req, res))) {
+      res.status(401);
+      return "Unauthorized";
+    }
+    // console.log("permissionsCheck");
     if (!this.permissionsCheck(req, res)) {
       res.status(403);
       return "Wrong permissions";
     }
 
     // check for missing required POST body fields
+    // console.log("send422: " + req.body.first_name);
+    // console.log("send422: " + req.body.last_name);
+    // console.log("send422: " + req.body.resume_url);
+    // console.log("send422: " + req.body.skills);
+
     let send422: string = "";
-    if (!req.body.first_name) send422 = "Missing first name as first_name";
-    if (!req.body.last_name) send422 = "Missing last name as last_name";
-    if (!req.body.resume_url) send422 = "Missing resume URL as resume_url";
-    if (!req.body.skills) send422 = "Missing skills as skills"; // TODO specify how to send SQL indices in POST body
+    if (!req.body.first_name) send422 += "Missing first name as first_name\n";
+    if (!req.body.last_name) send422 += "Missing last name as last_name\n";
+    if (!req.body.resume_url) send422 += "Missing resume URL as resume_url\n";
+    if (!req.body.skills) send422 += "Missing skills as skills\n"; // TODO specify how to send SQL indices in POST body
     if (send422) {
       res.status(422);
       return send422;
@@ -71,7 +111,14 @@ class UserController {
       project_preference: [],
     };
 
-    // TODO Telvin: add user object to DB
+    // TODO Telvin OR Brandon: Please Check the code below (Adding User to DB)
+    try{
+      await this.userRepository.save(user);
+      console.log("User has been saved");
+    } catch(e){
+      console.log("Error: "+ e);
+      // return e.message;
+    }
 
     return user;
   }

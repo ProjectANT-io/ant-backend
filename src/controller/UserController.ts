8238c46ffb7f34e0d7a67d5da34124e3b87e4f59
Patrick Louis <patricklouisw@gmail.com>
2020-11-11 19:39:58 -0500

    Finished User Routes

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,5 @@
 import { getRepository } from "typeorm";
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import User from "../entity/User";
 
 class UserController {
@@ -13,23 +13,6 @@ class UserController {
     return true; // TODO
   }
 
-  // async all(request: Request, response: Response, next: NextFunction) {
-  //   return this.userRepository.find();
-  // }
-
-  // async one(request: Request, response: Response, next: NextFunction) {
-  //   return this.userRepository.findOne(request.params.id);
-  // }
-
-  // async save(request: Request, response: Response, next: NextFunction) {
-  //   return this.userRepository.save(request.body);
-  // }
-
-  // async remove(request: Request, response: Response, next: NextFunction) {
-  //   let userToRemove = await this.userRepository.findOne(request.params.id);
-  //   // await this.userRepository.remove(userToRemove);
-  // }
-
   async newUser(req: Request, res: Response) {
     if (!(await this.authCheck(req, res))) {
       res.status(401);
@@ -41,39 +24,154 @@ class UserController {
     }
 
     // check for missing required POST body fields
-    let send422: string = "";
-    if (!req.body.first_name) send422 = "Missing first name as first_name";
-    if (!req.body.last_name) send422 = "Missing last name as last_name";
-    if (!req.body.resume_url) send422 = "Missing resume URL as resume_url";
-    if (!req.body.skills) send422 = "Missing skills as skills"; // TODO specify how to send SQL indices in POST body
-    if (send422) {
+    let missingFields: string = "";
+    ["first_name", "last_name", "resume_url", "skills"].forEach(
+      (expectedField) => {
+        if (!(expectedField in req.body)) {
+          missingFields += `Missing ${expectedField}\n`;
+        }
+      }
+    );
+    if (missingFields) {
       res.status(422);
-      return send422;
+      return missingFields;
+    }
+
+    // const firstName, lastName, resumeURL, skills
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const resumeURL = req.body.resume_url;
+    const { skills } = req.body;
+
+    let wrongType = "";
+    // Check for Correct Type of POST Body Fields, return 422 if type is not correct
+    if (typeof firstName !== "string") {
+      wrongType += `${typeof firstName}: first_name should be a string\n`;
+    }
+    if (typeof lastName !== "string") {
+      wrongType += `${typeof lastName}: last_name should be a string\n`;
+    }
+    if (typeof resumeURL !== "string") {
+      wrongType += `${typeof resumeURL}: resume_url should be a string\n`;
+    }
+    if (typeof skills !== "string") {
+      wrongType += `${typeof skills}: skills should be a string\n`;
+    }
+    if (wrongType) {
+      res.status(422);
+      return wrongType;
     }
 
     // TODO Telvin remove ESLint disable-line once DB code is in place
     // eslint-disable-next-line no-unused-vars
-    const user = {
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      status: "unverified", // TODO
-      about_me: "",
-      profile_picture_url: "",
-      resume_url: req.body.resume_url,
-      skills:
-        typeof req.body.skills === "string"
-          ? req.body.skills.split(",")
-          : req.body.skills,
-      external_urls: [],
-      educations: [],
-      previous_projects: [],
-      work_experiences: [],
-      project_preference: [],
-    };
+    // const user = {
+    //   first_name: req.body.first_name,
+    //   last_name: req.body.last_name,
+    //   status: "unverified", // TODO
+    //   about_me: "",
+    //   profile_picture_url: "",
+    //   resume_url: req.body.resume_url,
+    //   skills:
+    //     typeof req.body.skills === "string"
+    //       ? req.body.skills.split(",")
+    //       : req.body.skills,
+    //   external_urls: [],
+    //   educations: [],
+    //   previous_projects: [],
+    //   work_experiences: [],
+    //   project_preference: [],
+    // };
 
-    // TODO Telvin: add user object to DB
+    try {
+      const newUserInfo = this.userRepository.create(req.body);
+      const newUser = await this.userRepository.save(newUserInfo);
+      return newUser;
+    } catch (e) {
+      res.status(500);
+      return e;
+    }
+    // return user
+  }
 
-    return user;
+  async getUser(req: Request, res: Response) {
+    if (!(await this.authCheck(req, res))) {
+      res.status(401);
+      return "Unauthorized";
+    }
+    if (!this.permissionsCheck(req, res)) {
+      res.status(403);
+      return "Wrong permissions";
+    }
+
+    // Check for Required Path Parameter
+    if (!req.params.user_id) {
+      res.status(422);
+      return "Missing user_id as path parameter";
+    }
+
+    // Check for Correct Type of Required Path Parameter
+    const userID = Number(req.params.user_id);
+    if (Number.isNaN(userID)) {
+      res.status(422);
+      return "user_id should be a number";
+    }
+
+    // Get User in DB
+    try {
+      // Find User
+      const user = await this.userRepository.findOne(userID);
+
+      // If User Does Not Exist
+      if (!user) {
+        res.status(404);
+        return `User with ID ${userID} not found.`;
+      }
+
+      // Return Found User
+      return user;
+    } catch (e) {
+      res.status(500);
+      return e;
+    }
+  }
+
+  async updateUser(req: Request, res: Response) {
+    const user = await this.getUser(req, res);
+    if (res.statusCode !== 200) {
+      // calling this.getUser() returned an error, so return the error
+      return user;
+    }
+
+    // Update User in DB
+    try {
+      // Update & Return Found User
+      return await this.userRepository.save({
+        ...user, // retrieve existing properties
+        ...req.body, // override some existing properties
+      });
+    } catch (e) {
+      res.status(500);
+      return e;
+    }
+  }
+
+  async deleteUser(req: Request, res: Response) {
+    const user = await this.getUser(req, res);
+    if (res.statusCode !== 200) {
+      // calling this.getUser() returned an error, so return the error
+      return user;
+    }
+
+    try {
+      // Delete the User in DB
+      await this.userRepository.delete(user.id);
+
+      // Return the Deleted User
+      return user;
+    } catch (e) {
+      res.status(500);
+      return e;
+    }
   }
 }
 
---
+++ src/entity/IUser.ts
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+export default interface IUser {
+  id: number;
+  first_name: string;
+  last_name: string;
+  status: string;
+  about_me: string;
+  profile_picture_url: string;
+  resume_url: string;
+  // skills: ISkill[];
+  external_urls: string[];
+  // educations: IEducation[];
+  // previous_outside_projects: IPreviousOutsideProject[];
+  // work_experiences: IWorkExperience;
+  // project_preference: IEducation;
+}

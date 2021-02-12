ec273713f807fcd859dfcf16c3db41a6918bd2c5
Brandon <6111102+Firebird1029@users.noreply.github.com>
2021-02-11 18:17:39 -1000

    Replace static ormconfig.json with env vars

--- src/db_helper/db.ts
+++ src/db_helper/db.ts
@@ -18,8 +18,24 @@ export const DBConnect = async () => {
       await connection.connect();
     }
   } else {
-    const connectionOptions = await getConnectionOptions();
-    await createConnection(connectionOptions);
+    await createConnection({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: (process.env.DB_PORT as any) as number,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: false,
+      entities: ["src/entity/**/*.ts"],
+      migrations: ["src/migration/**/*.ts"],
+      subscribers: ["src/subscriber/**/*.ts"],
+      cli: {
+        entitiesDir: "src/entity",
+        migrationsDir: "src/migration",
+        subscribersDir: "src/subscriber",
+      },
+    });
   }
 };
 

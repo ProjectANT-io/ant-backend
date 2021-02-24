00bcfc9dd05ee1a40db37869a8c307b35185a5e3
Nate <nathanw88@gmail.com>
2021-02-23 20:30:32 -0600

    changed user and business type in authChecks from any to exact type expected

--- src/utils/authChecks.ts
+++ src/utils/authChecks.ts
@@ -1,10 +1,13 @@
-function checkUsersAuth(userLoggedin: any, userIdBeingAccessed: number) {
+function checkUsersAuth(
+  userLoggedin: { id: number },
+  userIdBeingAccessed: number
+) {
   if (userLoggedin.id === userIdBeingAccessed) return true;
   return false;
 }
 
 function checkUsersAuthForBusiness(
-  userLoggedin: any,
+  userLoggedin: { business: { id: number } },
   businessIdBeingAccessed: number
 ) {
   if (userLoggedin.business.id === businessIdBeingAccessed) return true;

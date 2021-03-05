d4bfbd8482163eec45a66f11f7ea19038f68c25f
Nate <nathanw88@gmail.com>
2021-03-04 19:36:59 -0600

    increased size limit on multer to 15 mb

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import { TryDBConnect } from "./db_helper/index";
 const multerMid = Multer({
   storage: Multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024,
+    fileSize: 15 * 1024 * 1024,
   },
 });
 
